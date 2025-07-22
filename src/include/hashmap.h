8af797d8836d796ace824794b9771bc6ac64742b
Hyp3rstatic <kazjrosinski@gmail.com>
2025-07-22 15:13:46 -0700

    updated code to work with lists

--- src/include/hashmap.h
+++ src/include/hashmap.h
@@ -5,16 +5,19 @@
 #include <string.h> //for string utilities
 
 /* defining the core functionality for a hashmap
-    keys will be chat pointers, and values will be void pointers
+    keys will be char pointers, and values will be void pointers
     being kept as simple as possible for now
 */
 
-//for determining types when assigning void pointers in functions
-typedef enum {
-  INT = 0,
-  FLOAT = 1,
-  STRING = 2,
-} types;
+//list node - singly linked
+//contains data and pointer to next
+typedef struct node_sl {
+  void *data;
+  struct node_sl *next;
+} node_sl;
+
+//list - points to a head - end of the list is a node pointing to NULL
+typedef node_sl *list_sl;
 
 /* struct for a key-value pair */
 /* intended only for use in a hashmap */
@@ -24,21 +27,34 @@ typedef struct {
   void *value;
 } kv_pair;
 
-//typedefing for code clarity
-typedef kv_pair **bucket;
-
 /* keys are unique, value are not */
 typedef struct {
-  bucket buckets; //an array of arrays to store k-v pairs within buckets
+  list_sl *buckets; //an array of lists used to store k-v pairs within buckets
   unsigned long size; //to keep track of size without needing to iterate
   unsigned long used; //keep track of buckets in use
 } hashmap;
+
+/* NODE FUNCTIONS */
+
+//return a new list node containing data
+node_sl *node_new(void *data);
+
+/* LINKED LIST FUNCTIONS */
+
+//return a new list
+list_sl list_new(void);
+
+//push - add an item to the end of a list
+unsigned short list_push(void *data, list_sl list);
+
+//remove - remove and item from the list by value
+unsigned short list_remove(unsigned long idx, list_sl list);
 
 /* KV_PAIR FUNCTIONS */
 
 //return memory address for a new kv_pair
 //type determines which cast is done on the value
-kv_pair *kv_pair_new(char *key, void *value, types type);
+kv_pair *kv_pair_new(char *key, void *value);
 
 /* HASHMAP FUNCTIONS */
 
@@ -66,27 +82,6 @@ unsigned short hashmap_clear(hashmap *map);
 //return the ratio of buckets used to hashmap size
 //for resizing checks
 float hashmap_ratio(hashmap *map);
-
-/* LINKED LIST */
-
-//list node - singly linked
-//contains data and pointer to next
-typedef struct node_sl {
-  void *data;
-  struct node_sl *next;
-} node_sl;
-
-//list - points to a head - end of the list is a node pointing to NULL
-typedef node_sl *list_sl;
-
-//return a new list
-list_sl list_new(void);
-
-//push - add an item to the end of a list
-unsigned short list_push(void *data, list_sl list);
-
-//remove - remove and item from the list by value
-unsigned short list_remove(unsigned long idx, list_sl list);
 
 #endif
 
